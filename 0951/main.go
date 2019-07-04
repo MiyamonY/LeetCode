4858d2c4f6a529650cef6a03277f40f77bd472d0
MIYAMOTO Yohei <y.miyamoto324@gmail.com>
2019-07-04 20:31:14 +0900

    Add 0951

---
+++ 0951/main.go
@@ -0,0 +1,32 @@
+// Package main provides
+//
+// File:  main.go
+// Author: ymiyamoto
+//
+// Created on Thu Jul  4 20:24:42 2019
+//
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func flipEquiv(root1 *TreeNode, root2 *TreeNode) bool {
+	if root1 == root2 {
+		return true
+	}
+	if root1 == nil || root2 == nil {
+		return false
+	}
+	if root1.Val != root2.Val {
+		return false
+	}
+
+	return (flipEquiv(root1.Left, root2.Left) && flipEquiv(root1.Right, root2.Right)) || (flipEquiv(root1.Left, root2.Right) && flipEquiv(root1.Right, root2.Left))
+}
+
+func main() {
+
+}
